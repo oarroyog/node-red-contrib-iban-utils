f316ee85d8e90a855e3ed087090514f39e5fcf3e
oarroyog <oscar.arroyo.gil.sa@everis.com>
2019-04-05 12:49:53 +0200

    Transformation error is showed

--- IbanUtils.js
+++ IbanUtils.js
@@ -21,10 +21,18 @@ module.exports = function(RED) {
                         for(var i=0;i<nodeContext.prop.length;i++){
                             if(IBAN.isValidBBAN(nodeContext.prop[i].countryCode, nodeContext.prop[i].bban)){
                                 nodeContext.prop[i].iban=IBAN.fromBBAN(nodeContext.prop[i].countryCode, nodeContext.prop[i].bban);
+                                nodeContext.prop[i].electronicFormat = IBAN.electronicFormat(nodeContext.prop[i].iban);
+                                nodeContext.prop[i].printFormat = IBAN.printFormat(nodeContext.prop[i].iban, nodeContext.prop[i].separator);                            
                                 nodeContext.prop[i].isValidIban=true;
                             }
                             else{
                                 nodeContext.prop[i].isValidIban=false;
+                                try{
+                                    IBAN.fromBBAN(nodeContext.prop[i].countryCode, nodeContext.prop[i].bban);
+                                }
+                                catch(e){
+                                    nodeContext.prop[i].err=e;
+                                }
                                 nodeContext.prop.returnError = true;
                             }
                         }
@@ -34,11 +42,19 @@ module.exports = function(RED) {
                         //Single conversion
                         if(IBAN.isValidBBAN(nodeContext.prop.countryCode, nodeContext.prop.bban)){
                             nodeContext.prop.iban=IBAN.fromBBAN(nodeContext.prop.countryCode, nodeContext.prop.bban);
-                            nodeContext.prop[i].isValidBban=true;
+                            nodeContext.prop.electronicFormat = IBAN.electronicFormat(nodeContext.prop.iban);
+                            nodeContext.prop.printFormat = IBAN.printFormat(nodeContext.prop.iban, nodeContext.prop.separator);                            
+                            nodeContext.prop.isValidIban=true;
                             node.send([msg,null]);
                         }
                         else{
                             nodeContext.prop.isValidBban=false;
+                            try{
+                                IBAN.fromBBAN(nodeContext.prop.countryCode, nodeContext.prop.bban);
+                            }
+                            catch(e){
+                                nodeContext.prop.err=e;
+                            }
                             node.send([null,msg]);
                         }
                     }
@@ -50,10 +66,18 @@ module.exports = function(RED) {
                         for(var i=0;i<nodeContext.prop.length;i++){
                             if(IBAN.isValid(nodeContext.prop[i].iban)){
                                 nodeContext.prop[i].bban=IBAN.toBBAN(nodeContext.prop[i].iban, nodeContext.prop[i].separator);
+                                nodeContext.prop[i].electronicFormat = IBAN.electronicFormat(nodeContext.prop[i].iban);
+                                nodeContext.prop[i].printFormat = IBAN.printFormat(nodeContext.prop[i].iban, nodeContext.prop[i].separator); 
                                 nodeContext.prop[i].isValidIban=true;
                             }
                             else{
                                 nodeContext.prop[i].isValidIban=false;
+                                try{
+                                    IBAN.toBBAN(nodeContext.prop[i].iban, nodeContext.prop[i].separator);
+                                }
+                                catch(e){
+                                    nodeContext.prop[i].err=e;
+                                }
                                 nodeContext.prop.returnError = true;
                             }
                         }
@@ -63,10 +87,18 @@ module.exports = function(RED) {
                         //Single conversion
                         if(IBAN.isValid(nodeContext.prop.iban)){
                             nodeContext.prop.bban = IBAN.toBBAN(nodeContext.prop.iban, nodeContext.prop.separator);
+                            nodeContext.prop.electronicFormat = IBAN.electronicFormat(nodeContext.prop.iban);
+                            nodeContext.prop.printFormat = IBAN.printFormat(nodeContext.prop.iban, nodeContext.prop.separator);
                             node.send([msg,null]);
                         }
                         else{
                             nodeContext.prop.isValidIban=false;
+                            try{
+                                IBAN.toBBAN(nodeContext.prop.iban, nodeContext.prop.separator);
+                            }
+                            catch(e){
+                                nodeContext.prop.err=e;
+                            }
                             node.send([null,msg]);
                         }
                     }            
